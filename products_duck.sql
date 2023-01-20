25aa3dedfe6dd748672fa515b7ef77874365a2a1
PerseP <persep@gmail.com>
2023-01-20 13:35:54 +0100

    Add ean and code to db

--- products_duck.sql
+++ products_duck.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS products(url VARCHAR, category VARCHAR, 
 	"name" VARCHAR, description VARCHAR, price DOUBLE, reference_price DOUBLE, 
-	reference_unit VARCHAR, date DATE, id DOUBLE, iva INTEGER);
+	reference_unit VARCHAR, date DATE, id VARCHAR, code VARCHAR, ean VARCHAR, iva INTEGER);
 
-insert into products(url, category, name, description, price, reference_price, reference_unit, date, id, iva)
+insert into products(url, category, name, description, price, reference_price, reference_unit, date, id, code, ean, iva)
 select
   json ->> '$.url',
   json ->> '$.category',
@@ -13,5 +13,7 @@ select
   json ->> '$.reference_unit',
   json ->> '$.date',
   json ->> '$.id',
+  json ->> '$.code',
+  json ->> '$.ean',
   json ->> '$.iva'
 from read_json_objects('temp.ndjson');
